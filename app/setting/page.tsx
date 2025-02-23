5e21b82a746ef5056724bcb5acb38963b984a8f3
szne <me@szn.jp>
2025-02-23 20:25:46 +0900

    Feat: レシピと設定のページを追加

---
+++ app/setting/page.tsx
@@ -0,0 +1,14 @@
+import Footer from '@/components/Footer'
+import styles from "./page.module.scss";
+
+
+export default function Home() {
+
+  return (
+    <>
+      <main className={styles.main}>
+      </main>
+      <Footer></Footer>
+    </>
+  );
+}
