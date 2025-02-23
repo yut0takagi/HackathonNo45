e60323f4bc0f31ccf7060365ab178f88d0632301
szne <me@szn.jp>
2025-02-23 17:58:50 +0900

    Feat: 新規登録頁を作成

---
+++ app/register/page.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+
+import styles from "./page.module.scss";
+import form from "@/components/form.module.scss"
+import Fridge from '@/components/fridge';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      {/* トップページ */}
+      <section className={styles.top}>
+        <img src="/tabedoki.svg" alt="タベドキのロゴ" />
+        <p>あなたの冷蔵庫を賢く管理🥕</p>
+      </section>
+      {/* ログインフォーム */}
+      <section className={form.form}>
+        <Fridge>
+          <h2>新規登録</h2>
+          <form action="/api/register" method="POST">
+            <input type="email" name="email" placeholder="メールアドレス" />
+            <input type="password" name="password" placeholder="パスワード" />
+            <input type="text" name="name" placeholder="名前" />
+            <button type="submit">登録する</button>
+          </form>
+          <p>アカウントをお持ちの方は<Link href="/">こちら</Link></p>
+        </Fridge>
+      </section>
+    </main>
+  );
+}
